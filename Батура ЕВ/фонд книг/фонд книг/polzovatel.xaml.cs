a260975b05d9403a4b727dff066fea05cf65be44
katirina233 <118421008+katirina233@users.noreply.github.com>
2023-09-14 17:28:37 +0900

    Add files via upload

---
+++ Батура ЕВ/фонд книг/фонд книг/polzovatel.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace фонд_книг
+{
+    /// <summary>
+    /// Логика взаимодействия для polzovatel.xaml
+    /// </summary>
+    public partial class polzovatel : Window
+    {
+        public polzovatel()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+            this close();
+        }
+    }
+}
